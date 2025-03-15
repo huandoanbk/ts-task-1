a2065b396456b295e157efa89548337fa96fa4e3
Huan <huandoan89@gmail.com>
2025-03-15 21:09:55 +0200

    first commit

---
+++ task-1.js
@@ -0,0 +1,6 @@
+function print(name, occu, wage) {
+    return "Hey, ".concat(name, " You are an ").concat(occu, " and your hourly wage is ").concat(wage, " dollars");
+}
+// Example usage:
+console.log(print("Sam", "Engineer", 25));
+// Output: Hey, Sam. You are an engineer and your hourly wage is 25 dollars.
---
+++ task-1.ts
@@ -0,0 +1,7 @@
+function print(name:string, occu:string, wage:number):string {
+  return `Hey, ${name} You are an ${occu} and your hourly wage is ${wage} dollars`;
+}
+
+// Example usage:
+console.log(print("Sam", "Engineer", 25));
+// Output: Hey, Sam. You are an engineer and your hourly wage is 25 dollars.
